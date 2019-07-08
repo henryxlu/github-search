bd417c216393dbe057820359e88dbc2b56bdc240
henryLu <henryxlu220@gmail.com>
2019-07-08 15:17:37 +0800

    done

---
+++ GithubSearch/Controller/ShowUserViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShowUserViewController.swift
+//  GithubSearch
+//
+//  Created by Henry on 2019/7/3.
+//  Copyright © 2019 Henry. All rights reserved.
+//
+
+import UIKit
+
+class ShowUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GithubSearch/view/UserDataTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserDataTableViewCell.swift
+//  GithubSearch
+//
+//  Created by Henry on 2019/7/3.
+//  Copyright © 2019 Henry. All rights reserved.
+//
+
+import UIKit
+
+class UserDataTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
